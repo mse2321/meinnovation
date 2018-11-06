ee65c7ea33bfd58c4fd300bb5687efcf7212f4ac
Marcus Eubanks <mse233@gmail.com>
2018-11-05 23:00:47 -0500

    added css from old site, setup webpack config, installed react-materialize and react-icons, created main.scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,38 @@
-var path = require('path');
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: 'development',
-  entry: './foo.js',
+  entry: '.src/index.js',
+  watch: true,
+  module: {
+	rules: [{
+        test: /\.scss$/,
+        use: [
+	        {
+	           process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
+	           options: {
+	            // you can specify a publicPath here
+	            // by default it use publicPath in webpackOptions.output
+	            publicPath: '.public/css/'
+	           }
+	        },
+            // fallback to style-loader in development
+            
+            "css-loader", // translates CSS into CommonJS
+            "sass-loader" // compiles Sass to CSS, using Node Sass by default
+        ]
+    }]
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'foo.bundle.js'
-  }
+    filename: 'main.js'
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+        // Options similar to the same options in webpackOptions.output
+        // both options are optional
+        chunkFilename: "main.css"
+    })
+  ]
 };
